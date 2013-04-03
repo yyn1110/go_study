87e7816cd4486d9d7cf089a28012d95d63373a62
yangyanan <yyn1110@163.com>
2013-04-03 11:58:38 +0800

    add tset array

--- go_study_1.go
+++ go_study_1.go
@@ -6,5 +6,12 @@ import (
 )
 
 func main() {
-	fmt.Println("Hello World!")
+	fmt.Println("**** test array ****")
+	arrayTest()
+}
+func arrayTest() {
+	var arr [10]int
+	arr[0] = 10
+	arr[1] = 20
+	fmt.Printf("the first element in arr is :%d\n", arr[0])
 }
